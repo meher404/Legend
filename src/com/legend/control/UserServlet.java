98edc42c0b73fb28f029a8c588bc20517d06b940
nikhithareddy02 <nikhithareddy02@gmail.com>
2014-02-09 16:14:06 +0530

    User and cart classes are modified

---
+++ src/com/legend/control/UserServlet.java
@@ -0,0 +1,31 @@
+package com.legend.control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class User
+ */
+@WebServlet("/User")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public UserServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
